65652f26b133ba922d8b37bb9d1491d6e2a0de82
Kudze <KudziokasChannel@gmail.com>
2016-07-13 23:42:33 +0300

    Removed debug message

--- FiveMP_Launcher/FiveMP_Client/Render.cpp
+++ FiveMP_Launcher/FiveMP_Client/Render.cpp
@@ -15,8 +15,6 @@ void CRender::RenderNametags()
 			float distanceXY = sqrtf(x*x + y*y);
 			float distance = sqrtf(distanceXY * distanceXY + z*z);
 
-			std::cout << distance << " | " << i << " | " << LocalPlayer->maxNickDrawDistance << std::endl;
-
 			if (distance < LocalPlayer->maxNickDrawDistance) {
 				GRAPHICS::_WORLD3D_TO_SCREEN2D(playerData[i].x, playerData[i].y, playerData[i].z + 2.0f, &playerData[i].screen_x, &playerData[i].screen_y);
 				draw_text(playerData[i].screen_x, playerData[i].screen_y, playerData[i].playerusername, { 255, 255, 255, 255 });
