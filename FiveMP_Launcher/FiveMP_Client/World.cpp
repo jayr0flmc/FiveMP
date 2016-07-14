6af997899b655a17784ce47f6c621d931504bc0b
Andy <nhm.nohalfmeasures@gmail.com>
2016-07-14 17:16:46 +0100

    Vehicle Cleanup on Disconnect

--- FiveMP_Launcher/FiveMP_Client/World.cpp
+++ FiveMP_Launcher/FiveMP_Client/World.cpp
@@ -10,4 +10,18 @@ void GameWorld::CleanUp() {
 			UI::REMOVE_BLIP(&playerData[i].pedBlip);
 		}
 	}
+
+	for (int i = 0; i < sizeof(vehicleData) / sizeof(*vehicleData); i++ ) {
+		if (ENTITY::DOES_ENTITY_EXIST(vehicleData[i].vehicleVehicle)) {
+			printf("Removing: %i\n", i);
+
+			ENTITY::DELETE_ENTITY(&vehicleData[i].vehicleVehicle);
+			UI::REMOVE_BLIP(&vehicleData[i].vehicleBlip);
+
+			vehicleData[i].used = false;
+		}
+		else {
+			printf("Failed Removing: %i\n", i);
+		}
+	}
 }
