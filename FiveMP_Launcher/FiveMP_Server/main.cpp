b3d80e5a808be96a35bca4e27c8a1707596a4977
Kudze <KudziokasChannel@gmail.com>
2016-07-20 15:44:31 +0300

    Added IsPlayerInAnyVehicle

--- FiveMP_Launcher/FiveMP_Server/main.cpp
+++ FiveMP_Launcher/FiveMP_Server/main.cpp
@@ -75,6 +75,9 @@ int main(void)
 	lua_register(sLUA, "SetPlayerComponentVariation", SetPedComponentVariation);
 	lua_register(sLUA, "SetPlayerControlable", SetPlayerControlable);
 
+	lua_register(sLUA, "IsPlayerInAnyVehicle", IsPlayerInAnyVehicle);
+	lua_register(sLUA, "IsPlayerInVehicle", IsPlayerInVehicle);
+
 	// Player (UI)
 	lua_register(sLUA, "ShowMessageToPlayer", ShowMessageToPlayer);
 	lua_register(sLUA, "ShowMessageToAll", ShowMessageToAll);
